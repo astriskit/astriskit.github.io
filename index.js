7350fc2ca812b51401edff2bb5950a74359dbb71
Hariom Soni <hariomsoni@ninja.local>
2019-11-18 12:34:37 +0530

    added color effects on row and  link; added section elements; moved loading-alert outside of root element;

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function setInfo(
     }</td></tr>`;
   });
   tbody.innerHTML = rows;
-  e("repos").style.display = "unset";
+  e("root").style.display = "unset";
   e("loading-alert").style.display = "none";
 }
 
